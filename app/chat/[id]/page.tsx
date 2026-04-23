439c92f3c09050bd1d67a0b666622cd25fc915b5
Itscarterjunior <160657064+Itscarterjunior@users.noreply.github.com>
2026-04-23 13:21:13 +0200

    Delete app directory

--- app/chat/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-// Chat screen with messaging
-import React from 'react';
-
-const ChatPage = () => {
-  return <h1>Chat Screen</h1>;
-};
-
-export default ChatPage;
--- app/nearby/page.tsx
+++
@@ -1,8 +0,0 @@
-// Nearby people listing page
-import React from 'react';
-
-const NearbyPage = () => {
-  return <h1>Nearby People</h1>;
-};
-
-export default NearbyPage;
